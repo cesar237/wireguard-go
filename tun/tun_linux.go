f1155c353557703e161b3f5ed5cdf906e2d13109
Cesar MH <56159783+cesar237@users.noreply.github.com>
2024-08-24 14:46:21 +0200

    Test

--- tun/tun_linux.go
+++ tun/tun_linux.go
@@ -570,7 +570,7 @@ func CreateTUN(name string, mtu int) (Device, error) {
 	// IFF_VNET_HDR enables the "tun status hack" via routineHackListener()
 	// where a null write will return EINVAL indicating the TUN is up.
 	ifr.SetUint16(unix.IFF_TUN | unix.IFF_NO_PI | unix.IFF_VNET_HDR | unix.IFF_MULTI_QUEUE | unix.IFF_NAPI | unix.IFF_ATTACH_QUEUE)
-	err = unix.IoctlIfreq(nfd, unix.TUNSETIFF | unix.TUNSETQUEUE, ifr)
+	err = unix.IoctlIfreq(nfd, unix.TUNSETIFF, ifr)
 	if err != nil {
 		fmt.Printf("The problem is Here... (NumCPU=%d)\n", runtime.NumCPU())
 		return nil, err
